5a5f23be6406e93bf5266abaa11bf636b6e71fde
evilblubb <brian.barnhart@in-focus.net>
2020-02-18 09:50:00 +0100

    Added Comments

--- thingspeak2mySQL.py
+++ thingspeak2mySQL.py
@@ -7,16 +7,16 @@ import mysql.connector as mariadb
 result={}
 mariadb_connection = mariadb.connect(user='', password='', host='', database='')
 cursor = mariadb_connection.cursor()
-cursor.execute("SELECT * FROM HUM ORDER BY ID DESC LIMIT 1")
+cursor.execute("SELECT * FROM HUM ORDER BY ID DESC LIMIT 1") #HUM = DB Name
 records = cursor.fetchall()
 records = list(records[0])
-records=records[2]
+records = records[2]
 
-ch = thingspeak.Channel(757628)
+ch = thingspeak.Channel() #ChanelID
 
-data1 = ch.get_field(field=1)
-data2 = ch.get_field(field=2)
-data3 = ch.get_field(field=3)
+data1 = ch.get_field(field=1) #weight
+data2 = ch.get_field(field=2) #temp
+data3 = ch.get_field(field=3) #humidity
 
 parsed1 = json.loads(data1)
 parsed2 = json.loads(data2)
@@ -26,6 +26,7 @@ data1 = parsed1['feeds']
 data2 = parsed2['feeds']
 data3 = parsed3['feeds']
 
+#Write new weight Data 
 for i in data1:
     a = i['entry_id']
     b = i['created_at']
@@ -35,11 +36,11 @@ for i in data1:
     else:
         if i['field1'] is not None:
             result[a] = float(i['field1'])
-            cursor.execute("INSERT INTO Gewicht (TIMESTAMP, KG) VALUES (%s,%s)",(b, result[a]))
+            cursor.execute("INSERT INTO WEIGHT (TIMESTAMP, KG) VALUES (%s,%s)",(b, result[a])) #change WEIGHT according to your DB
             mariadb_connection.commit()
         else:
             pass
-
+#Write new temperature Data 
 for i in data2:
     a = int(i['entry_id'])
     b = i['created_at']
@@ -49,10 +50,13 @@ for i in data2:
     else:
         if i['field2'] is not None:
             result[a] = float(i['field2'])
-            cursor.execute("INSERT INTO TEMP (TIMESTAMP, TEMP) VALUES (%s,%s)",(b, result[a]))
+            cursor.execute("INSERT INTO TEMP (TIMESTAMP, TEMP) VALUES (%s,%s)",(b, result[a])) #change TEMP according to your DB
             mariadb_connection.commit()
         else:
             pass
+        
+        
+#Write new humidity Data         
 for i in data3:
     a = int(i['entry_id'])
     b = i['created_at']
@@ -62,7 +66,7 @@ for i in data3:
     else:
         if i['field3'] is not None:
             result[a] = float(i['field3'])
-            cursor.execute("INSERT INTO HUM (TIMESTAMP, HUM, ID) VALUES (%s,%s,%s)",(b, result[a], a))
+            cursor.execute("INSERT INTO HUM (TIMESTAMP, HUM, ID) VALUES (%s,%s,%s)",(b, result[a], a)) #change HUM according to your DB
             mariadb_connection.commit()
         else:
             pass
